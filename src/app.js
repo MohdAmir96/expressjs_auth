d6a27d09aaf97cbd4347f1d4eed79a67dc5ff428
Mohd Amir <amirmanawwar@gmail.com>
2025-05-19 02:59:41 +0530

    added cross origin for fe

--- src/app.js
+++ src/app.js
@@ -10,7 +10,8 @@ const allowedOrigins = [
   "http://localhost:3000",
   "http://localhost:3001",
   "http://127.0.0.1:5173",
-  "https://expressjs-auth.onrender.com",
+  // "https://expressjs-auth.onrender.com",
+  "https://naukrprovider.netlify.app/login",
 ];
 
 // Dynamic CORS configuration
