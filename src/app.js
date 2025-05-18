55eefcf2ad659f8c36770f2d0fbef7eb2fd01d0d
Mohd Amir <amirmanawwar@gmail.com>
2025-05-19 03:14:38 +0530

    minor fix

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ const allowedOrigins = [
   "http://localhost:3001",
   "http://127.0.0.1:5173",
   // "https://expressjs-auth.onrender.com",
-  "https://naukrprovider.netlify.app/login",
+  "https://naukrprovider.netlify.app/",
 ];
 
 // Dynamic CORS configuration
