899009d1710eab94e0c889ed73b9b0b3aacaa4e8
Mohd Amir <amirmanawwar@gmail.com>
2025-05-18 23:18:23 +0530

    configured cors

--- src/app.js
+++ src/app.js
@@ -1,9 +1,16 @@
 const express = require("express");
+const cors = require("cors");
 const authRoutes = require("./routes/auth.routes");
 
 const app = express();
+
+// Enable CORS for all routes
+app.use(cors());
+
+// Parse incoming JSON requests
 app.use(express.json());
 
+// Routes
 app.use("/api/auth", authRoutes);
 
 module.exports = app;
