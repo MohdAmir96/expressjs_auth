833a3447d44e81bdc8f6ac7aa9e349bf45839667
Mohd Amir <amirmanawwar@gmail.com>
2025-05-19 01:35:41 +0530

    minor fix

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -5,7 +5,7 @@ const bcrypt = require("bcrypt");
 const COOKIE_OPTIONS = {
   httpOnly: true,
   secure: process.env.NODE_ENV === "production",
-  sameSite: "Strict",
+  sameSite: "None",
   maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
 };
 
